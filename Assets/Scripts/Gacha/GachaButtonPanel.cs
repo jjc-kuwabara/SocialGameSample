8c70aab92428d693237bdc302a403c45e396c65b
jjc-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2022-10-31 00:32:58 +0900

    ガチャ演出と編成画面完成

--- Assets/Scripts/Gacha/GachaButtonPanel.cs
+++ Assets/Scripts/Gacha/GachaButtonPanel.cs
@@ -18,6 +18,9 @@ public class GachaButtonPanel : MonoBehaviour
         UserDataManager userDataManager = GameObject.Find("UserDataManager").GetComponent<UserDataManager>();
         int currentUserId = int.Parse(userDataManager.GetCurrentUserId());
         userDataManager.CallGachaPointSub1(currentUserId);
+
+        GachaPerformManager gachaPerformManager = GameObject.Find("GachaPerformManager").GetComponent<GachaPerformManager>();
+        gachaPerformManager.CallGacha1();
     }
 
     public void OnClickGachaPointSub10Button()
@@ -25,5 +28,8 @@ public class GachaButtonPanel : MonoBehaviour
         UserDataManager userDataManager = GameObject.Find("UserDataManager").GetComponent<UserDataManager>();
         int currentUserId = int.Parse(userDataManager.GetCurrentUserId());
         userDataManager.CallGachaPointSub10(currentUserId);
+
+        GachaPerformManager gachaPerformManager = GameObject.Find("GachaPerformManager").GetComponent<GachaPerformManager>();
+        gachaPerformManager.CallGacha10();
     }    
 }
--- Assets/Scripts/UserDataManager.cs
+++ Assets/Scripts/UserDataManager.cs
@@ -17,11 +17,21 @@ public class UserDataManager : SingletonMonoBehaviour<UserDataManager>
 
     public void CallUserData_LobbySystem(int userId)
     {
-        string url = "http://35.79.36.5/PHPFolder/Jugyo/GetUserDataFromUserId.php?userId=" + userId;
+        {
+            string url = "http://35.79.36.5/PHPFolder/Jugyo/GetUserDataFromUserId.php?userId=" + userId;
+            StartCoroutine(GetUserData(url, ()=>RefreshLobbySystemUI()));
+            RefreshLobbySystemUI();
+        }
 
-        StartCoroutine(GetUserData(url, ()=>RefreshLobbySystemUI()));
-        RefreshLobbySystemUI();
-    }    
+        CallRefreshUserDataAdd(userId);
+    }
+
+    public void CallRefreshUserDataAdd(int userId){
+        {
+            string url = "http://35.79.36.5/PHPFolder/Jugyo/GetUserDataAddFromUserId.php?userId=" + userId;
+            StartCoroutine(GetUserDataAdd(url, ()=>RefreshUserDataAddUI()));
+        }
+    }
 
     public void CallAddPoint(int userId)
     {
@@ -48,21 +58,20 @@ public class UserDataManager : SingletonMonoBehaviour<UserDataManager>
     {
         string url = "http://35.79.36.5/PHPFolder/Jugyo/SubPoint_Gacha1.php?userId=" + userId;
 
-        StartCoroutine(UrlAccess(url, ()=>CallUserData_LobbySystem(userId)));
+        StartCoroutine(GachaAccess(url, ()=>CallUserData_LobbySystem(userId)));
     }
 
     public void CallGachaPointSub10(int userId)
     {
         string url = "http://35.79.36.5/PHPFolder/Jugyo/SubPoint_Gacha10.php?userId=" + userId;
 
-        StartCoroutine(UrlAccess(url, ()=>CallUserData_LobbySystem(userId)));
+        StartCoroutine(GachaAccess(url, ()=>CallUserData_LobbySystem(userId)));
     }
 
     public void CallHasCharaFlagReset(int userId){
         string url = "http://35.79.36.5/PHPFolder/Jugyo/ResetHasCharaFlag.php?userId=" + userId;
 
-        StartCoroutine(UrlAccess(url));
-
+        StartCoroutine(UrlAccess(url, ()=>CallRefreshUserDataAdd(userId)));
     }
 
     // ユーザデータを取得してきて、メンバ変数に格納する.
@@ -92,16 +101,49 @@ public class UserDataManager : SingletonMonoBehaviour<UserDataManager>
         }
     }
 
-    private void RefreshRendererSampleUI(){
-            UserDataRenderer userDataRenderer = GameObject.Find("UserDataRenderer").GetComponent<UserDataRenderer>();
-            userDataRenderer.Refresh(m_userId, m_name, m_pass, m_point);
-    }
+    IEnumerator GetUserDataAdd(string url, UnityAction callbackFunc = null)
+    {
+        WWWForm form = new WWWForm();
+        using (WWW www = new WWW(url, form))
+        {
+            yield return www;
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log("error:" + www.error);
+                yield break;
+            }
+            Debug.Log("text:" + www.text);
 
-    private void RefreshLobbySystemUI(){
-        LobbyUserDataRenderer lobbyUserDataRenderer = GameObject.Find("LobbyUserDataRenderer").GetComponent<LobbyUserDataRenderer>();
-        lobbyUserDataRenderer.RefreshUserData(m_name);
-        LobbyGachaPointRenderer lobbyGachaPointRenderer = GameObject.Find("LobbyGachaPointRenderer").GetComponent<LobbyGachaPointRenderer>();
-        lobbyGachaPointRenderer.RefreshGachaPoint(m_point);
+            string resultText = www.text;
+            string[] resultTextArray = resultText.Split(",");
+            m_exp = resultTextArray[1];
+            m_hasCharaFlag = resultTextArray[2];
+
+            if(callbackFunc != null){
+                callbackFunc();
+            }
+        }
+    }    
+
+    IEnumerator GachaAccess(string url, UnityAction callbackFunc = null)
+    {
+        WWWForm form = new WWWForm();
+        using (WWW www = new WWW(url, form))
+        {
+            yield return www;
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.Log("error:" + www.error);
+                yield break;
+            }
+            Debug.Log("text:" + www.text);
+
+            m_currentGachaResult = www.text;
+
+            if(callbackFunc != null){
+                callbackFunc();
+            }
+        }
     }
 
     // 単純に当該のURLにアクセスする.
@@ -140,13 +182,57 @@ public class UserDataManager : SingletonMonoBehaviour<UserDataManager>
         }
     }
 
+
+    private void RefreshRendererSampleUI(){
+            UserDataRenderer userDataRenderer = GameObject.Find("UserDataRenderer").GetComponent<UserDataRenderer>();
+            userDataRenderer.Refresh(m_userId, m_name, m_pass, m_point);
+    }
+
+    private void RefreshLobbySystemUI(){
+        LobbyUserDataRenderer lobbyUserDataRenderer = GameObject.Find("LobbyUserDataRenderer").GetComponent<LobbyUserDataRenderer>();
+        lobbyUserDataRenderer.RefreshUserData(m_name);
+        LobbyGachaPointRenderer lobbyGachaPointRenderer = GameObject.Find("LobbyGachaPointRenderer").GetComponent<LobbyGachaPointRenderer>();
+        lobbyGachaPointRenderer.RefreshGachaPoint(m_point);
+    }
+
+    private void RefreshUserDataAddUI(){
+        RefreshLobbyPartyUI();
+        RefreshLobbyGachaPerformUI();
+    }
+
+    private void RefreshLobbyPartyUI(){
+        GameObject obj = GameObject.Find("PartyCharaGridRenderer");
+        if(obj != null){
+            PartyCharaGridRenderer partyCharaGridRenderer = obj.GetComponent<PartyCharaGridRenderer>();
+            partyCharaGridRenderer.RefreshGrid();
+        }
+    }    
+    private void RefreshLobbyGachaPerformUI(){
+        GameObject obj = GameObject.Find("GachaPerformManager");
+        if(obj != null){
+            GachaPerformManager gachaPerformManager = obj.GetComponent<GachaPerformManager>();
+            gachaPerformManager.OnFinishPlayGacha();
+        }
+    }   
+
     public string GetCurrentUserId(){
         return m_userId;
     }
 
+    public string GetCurrentHasCharaFlag(){
+        return m_hasCharaFlag;
+    }
+
+    public string GetCurrentGachaResult(){
+        return m_currentGachaResult;
+    }
+
     // UserDataManagerクラスのメンバ変数の宣言.
-    string m_userId = "";
+    string m_userId = "0";
     string m_name = "";
     string m_pass = "";
-    string m_point = "";
+    string m_point = "0";
+    string m_exp = "0";
+    string m_hasCharaFlag = "0";
+    string m_currentGachaResult = "";
 }
